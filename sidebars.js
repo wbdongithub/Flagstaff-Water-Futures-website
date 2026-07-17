0d4d897ab22e806a75216416c10e2f0d6f447efe
wbdongithub <30533779+wbdongithub@users.noreply.github.com>
2026-07-17 10:52:40 -0700

    Add files via upload

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,56 @@
 module.exports = {
   waterLibrary: [
     'overview',
-    {type: 'category', label: 'Flagstaff Today', items: ['flagstaff-water-story','current-water-system','future-water-needs']},
-    {type: 'category', label: 'The Alternatives', items: ['awt-overview','rgr-overview','comparison-framework']},
-    {type: 'category', label: 'Key Issues', items: ['water-quality','funding-and-politics','technology-change','risk-and-uncertainty']},
-    {type: 'category', label: 'Project Resources', items: ['history', 'glossary', 'sources','questions-to-answer']},
+    {
+      type: 'category',
+      label: 'Flagstaff Today',
+      items: [
+        'flagstaff-water-story',
+        'current-water-system',
+        'future-water-needs',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'The Alternatives',
+      items: [
+        'awt-overview',
+        'rgr-overview',
+        'comparison-framework',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Key Issues',
+      items: [
+        'water-quality',
+        'funding-and-politics',
+        'technology-change',
+        'risk-and-uncertainty',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Glossary',
+      collapsed: false,
+      items: [
+        'glossary/overview',
+        'glossary/a-d',
+        'glossary/e-l',
+        'glossary/m-r',
+        'glossary/s-z',
+        'glossary/acronyms',
+        'glossary/units',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Project Resources',
+      items: [
+        'history',
+        'sources',
+        'questions-to-answer',
+      ],
+    },
   ],
 };
