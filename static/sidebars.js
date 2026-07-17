2489221029c96bf2a908e5061d427839ea23b55f
wbdongithub <30533779+wbdongithub@users.noreply.github.com>
2026-07-17 12:57:23 -0700

    Add 'glossary' item to Project Resources category

--- static/sidebars.js
+++ static/sidebars.js
@@ -4,6 +4,6 @@ module.exports = {
     {type: 'category', label: 'Flagstaff Today', items: ['flagstaff-water-story','current-water-system','future-water-needs']},
     {type: 'category', label: 'The Alternatives', items: ['awt-overview','rgr-overview','comparison-framework']},
     {type: 'category', label: 'Key Issues', items: ['water-quality','funding-and-politics','technology-change','risk-and-uncertainty']},
-    {type: 'category', label: 'Project Resources', items: ['history','sources','questions-to-answer']},
+    {type: 'category', label: 'Project Resources', items: ['history', 'glossary', 'sources','questions-to-answer']},
   ],
 };
